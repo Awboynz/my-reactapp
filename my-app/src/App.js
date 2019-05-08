a150c9d190dcade37776d8c7f6cde980a6d4dc1c
AWP Poprakod <Naoki_Boy@outlook.com>
2019-05-08 16:26:32 +0700

    About
    About Page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,25 +3,24 @@ import './App.css'
 import React , {Component} from "react"
 import { BrowserRouter, Route, Switch} from "react-router-dom"
 import Home from "./containers/Home"
+import About from "./containers/About"
 
 
 class App extends Component {
 
     constructor(props) {
       super(props)
-
-      
     }
 
     renderRouter(){
       return(
         <Switch>
-            <Route path="/" component={Home}></Route>
+            <Route exact path="/" component={Home}></Route>
+            <Route exact path="/about" component={About}></Route>
         </Switch>
 
       )
     }
-
     render(){
       return(
         <BrowserRouter>{this.renderRouter()}</BrowserRouter>
---
+++ my-app/src/containers/About.js
@@ -0,0 +1,19 @@
+import React from "react"
+import Header from "../components/Header"
+import Footer from "../components/Footer"
+
+const About = () => {
+return(
+    <div>
+        <Header />
+            <div className="container col-md-5">
+                <h3>เกี่ยวกับเรา</h3>
+                <p>Test Description</p>
+                <h4>Test Description</h4>
+            </div>
+        <Footer username= "Assadawoot Poprakod" email="awboy005@gmail.com"/>
+    </div>
+
+)
+}
+export default About
